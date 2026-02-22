c6adf7a7fe8403b26b4782f2555a61fc194fd615
Seraphin Eyala <eyalas472@gmail.com>
2026-02-22 16:17:39 +0100

    Test step_03 passer avec succees

--- tp1_socle_fastapi_Diarra/app/services/users_service.py
+++ tp1_socle_fastapi_Diarra/app/services/users_service.py
@@ -1,8 +1,7 @@
-from typing import List, Optional
+from typing import List
 from app.models.user_model import UserModel
 from app.models.user_model_create import UserModelCreate
-from app.services.users_service_protocol import IUsersService
-from app.factories.users_factory_protocol import IUsersFactory
+from app.repositories.protocols.i_users_repository import IUsersRepository
 
 class UsersService:
 
@@ -14,29 +13,16 @@ class UsersService:
     - persistance en mémoire (volontairement simple)
     """
 
-    def __init__(self, factory: IUsersFactory, users_json_path: str):
-        self.factory = factory
-        self._users: list[UserModel] = factory.create_users(users_json_path)
+    def __init__(self, repository: IUsersRepository):
+        self.repository = repository
 
 
     def create_user(self, user: UserModelCreate) -> UserModel:
-        next_id = max(user.id for user in self._users) + 1
-
-        created_user = UserModel(
-            id=next_id,
-            login=user.login,
-            age=user.age
-        )
-
-        self._users.append(created_user)
-        return created_user
+        return self.repository.create_user(user)
 
 
-    def get_user_by_id(self, user_id: int) -> Optional[UserModel]:
-        for user in self._users:
-            if user.id == user_id:
-                return user
-        return None
+    def get_user_by_id(self, user_id: int) -> UserModel | None:
+       return self.repository.get_user_by_id(user_id)
 
     def list_users(self) -> List[UserModel]:
-        return list(self._users)
+        return self.repository.list_users()
