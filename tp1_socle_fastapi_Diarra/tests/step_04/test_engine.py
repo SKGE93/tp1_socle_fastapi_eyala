16859749b91e5b48732e32653e7a98395123fca2
Seraphin Eyala <eyalas472@gmail.com>
2026-02-22 18:54:26 +0100

    test parteii 2 etape 4 passe avec succees

---
+++ tp1_socle_fastapi_Diarra/tests/step_04/test_engine.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from sqlalchemy import text
+
+from app.db.engine import get_engine
+
+def test_should_execute_select_1_given_engine():
+    # Arrange
+    engine = get_engine()
+
+    # Act
+    with engine.connect() as c:
+        result = c.execute(text("SELECT 1")).scalar_one()
+
+    # Assert (1 assertion métier)
+    assert result == 1
